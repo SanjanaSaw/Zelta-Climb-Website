07e8ddf479b05d15235f4402b24196d1799014e9
sanjana6923 <sanjanassaw@gmail.com>
2024-12-29 18:52:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const specializationItems = document.querySelectorAll('.specialization-item');
+
+specializationItems.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.style.transform = 'scale(1.05)';
+  });
+
+  item.addEventListener('mouseout', () => {
+    item.style.transform = 'scale(1)';
+  });
+});
